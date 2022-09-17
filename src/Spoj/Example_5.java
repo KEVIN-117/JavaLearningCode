2c8342cd763c9795ca250573c8326ffad44defcc
KEVIN-117 <sis414.kevinrodriguez17@gmail.com>
2022-09-17 18:21:26 -0400

    new

--- src/Spoj/Example_5.java
+++ src/Spoj/Example_5.java
@@ -6,6 +6,5 @@ public class Example_5 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-
     }
 }
---
+++ src/Spoj/Example_7.java
@@ -0,0 +1,7 @@
+package Spoj;
+
+public class Example_7 {
+    public static void main(String[] args) {
+
+    }
+}
